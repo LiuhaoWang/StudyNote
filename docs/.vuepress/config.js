363a210d64111c99473abf04ec071a5acd2e379e
“liuhaowang” <664421190@qq.com>
2022-07-25 21:56:39 +0800

    项目初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  themeConfig: {
+    
+  },
+  smoothScroll: true,
+  sidebar: 'auto',
+  title: "Hello VuePress",
+  description: "Just playing around",
+  search: true,
+  searchMaxSuggestions: 10
+};
